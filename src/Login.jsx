97c06f8a318013b104a73481b73e2675cbff4389
Nischay Kuwala <nischaykuwala@gmail.com>
2022-11-19 13:48:46 +0530

    custom HOC is used

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,33 +1,36 @@
-import { useFormik } from "formik";
+import { withFormik } from "formik";
 import React from "react";
 import { Link } from "react-router-dom";
 import * as yup from "yup";
 
-function Login() {
-  function callloginapi(values) {
-    console.log("email & password", values.email, values.password);
-  }
-  const schema = yup.object().shape({
-    email: yup.string().required().email("Please enter a valid Email Address"),
-    password: yup
-      .string()
-      .required()
-      .min(10, "Password must be at least 10 characters long"),
-  });
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    onSubmit: callloginapi,
-    validationSchema: schema,
-  });
+function callloginapi(values) {
+  console.log("email & password", values.email, values.password);
+}
+const schema = yup.object().shape({
+  email: yup.string().required().email("Please enter a valid Email Address"),
+  password: yup
+    .string()
+    .required()
+    .min(10, "Password must be at least 10 characters long"),
+});
 
+const initialValues = {
+  email: "",
+  password: "",
+};
+export function Login({
+  handleBlur,
+  handleChange,
+  touched,
+  errors,
+  handleSubmit,
+  values,
+}) {
   return (
     <>
       <div className="flex justify-center h-full w-full">
         <form
-          onSubmit={formik.handleSubmit}
+          onSubmit={handleSubmit}
           className="flex flex-col place-items-center h-98 w-100 p-8 bg-blue-400 rounded-lg my-8 shadow-2xl shadow-blue-700"
         >
           <div className="flex justify-center font-extrabold text-red-500 text-2xl my-4 shadow-2xl shadow-blue-400">
@@ -37,9 +40,9 @@ function Login() {
             Email
           </label>
           <input
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
             className="font-semibold  border border-solid mb-2 w-40 rounded-md"
             type="email"
             required
@@ -48,18 +51,18 @@ function Login() {
             name="email"
             placeholder=" Email Address"
           />
-          {formik.touched.email && formik.errors.email && (
+          {touched.email && errors.email && (
             <div className="text-red-500 font-semibold underline mb-2">
-              {formik.errors.email}
+              {errors.email}
             </div>
           )}
           <label htmlFor="password" className="font-bold">
             Password
           </label>
           <input
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
             className="font-semibold  border border-solid mb-2 w-40 rounded-md"
             type="password"
             id="password"
@@ -68,9 +71,9 @@ function Login() {
             autoComplete="current-password"
             required
           />
-          {formik.touched.password && formik.errors.password && (
+          {touched.password && errors.password && (
             <div className="text-red-500 font-semibold underline mb-2">
-              {formik.errors.password}
+              {errors.password}
             </div>
           )}
           <button
@@ -93,4 +96,12 @@ function Login() {
     </>
   );
 }
-export default Login;
+const myHOC = withFormik({
+  handleSubmit: callloginapi,
+  initialValues: initialValues,
+  validationSchema: schema,
+});
+
+const Easylogin = myHOC(Login);
+
+export default Easylogin;
